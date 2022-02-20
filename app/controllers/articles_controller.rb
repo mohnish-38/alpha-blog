4da89df00f10e9e08dd77a30f428a615e860fbc7
Mohnish Jharbade <jharbadeji38@gmail.com>
2022-02-20 14:11:05 +0530

    Apply DRY in code and make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+   before_action :set_article, only: [:show, :edit, :update, :destroy]
    
    def show
-      @article = Article.find(params[:id])
+      
    end
 
    def index
@@ -13,15 +14,15 @@ class ArticlesController < ApplicationController
    end
 
    def edit
-      @article = Article.find(params[:id])
+      
    end
 
   
 
    def create
-      @article = Article.new(params.require(:article).permit(:title, :description))
+      @article = Article.new(article_params)
       if @article.save
-        flash[:notice] = "Article was created successfully."
+        
         redirect_to @article 
       else
         render :new, status: :unprocessable_entity
@@ -29,8 +30,8 @@ class ArticlesController < ApplicationController
    end
 
    def update
-      @article = Article.find(params[:id])
-     if @article.update(params.require(:article).permit(:title, :description))
+      
+     if @article.update(article_params)
         redirect_to @article
      else
         render :edit, status: :unprocessable_entity
@@ -38,10 +39,17 @@ class ArticlesController < ApplicationController
    end
 
    def destroy
-      @article = Article.find(params[:id])
+      
       @article.destroy
       redirect_to articles_path, status: :see_other
    end
-    
+
+   private
+   def set_article
+     @article = Article.find(params[:id])
+   end 
+   def article_params
+      params.require(:article).permit(:title, :description)
+   end
 
 end   
